18afb0829c25bb0b46e23289d383f2abb0c1aa02
Siva Munaganuru <smunagan@asu.edu>
2024-01-19 15:56:53 -0700

    Initial Commit

---
+++ recipeApp/recipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeAppApp.swift
+//  recipeApp
+//
+//  Created by Siva Munaganuru on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct recipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
